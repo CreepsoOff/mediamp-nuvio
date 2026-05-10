9645463ba90e5efbe0318a3a4a25d8627ae07a52
CreepsoOff <51055703+CrepsoOff@users.noreply.github.com>
2026-05-10 22:21:18 +0200

    fix(mpv): compile SG C++ DLL with FBO unbind fix
    The SG C++ render_frame_to_texture already unbinds the FBO after
    
    mpv_render_context_render (pfnGlBindFramebuffer(GL_FRAMEBUFFER, 0)).
    
    The old prebuilt DLL from build-ci lacked this unbind, causing
    
    drawImage() to render into the FBO instead of the screen.
    
    Build: cmake + Ninja with VS 2026 BuildTools.
    
    Output: build_cmake/mediampv.dll copied to build-ci/

---
+++ mediamp-mpv/build_dll.bat
@@ -0,0 +1,8 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64
+cd /d "%~dp0"
+if not exist build_cmake mkdir build_cmake
+cd build_cmake
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release
+cmake --build . --config Release
+echo BUILD COMPLETE
